21172a3757cbf52ecb5359b5404f6b7034055eca
Loic Rossi <loic_2610@hotmail.fr>
2023-03-01 11:54:28 +0100

    starting project

---
+++ src/components/CustomForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CustomForm = () => {
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    console.log(e)
+
+  return (
+    <form className='todo'
+    onSubmit={handleFormSubmit}>
+      <div className='wrapper'>
+        
+      </div>
+    </form>
+  )
+}
+
+export default CustomForm
