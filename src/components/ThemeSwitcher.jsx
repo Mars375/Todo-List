2343022c536929190c62f03c891bdbe01bf718ed
Loic Rossi <loic_2610@hotmail.fr>
2023-03-01 16:03:53 +0100

    Add Custom theme Switcher

---
+++ src/components/ThemeSwitcher.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react'
+
+// Icons
+import { XMarkIcon, SunIcon, MoonIcon, SwatchIcon } from '@heroicons/react/24/outline'
+
+// styles
+import styles from './ThemeSwitcher.module.css'
+
+const ThemeSwitcher = () => {
+  const [hue, setHue] = useState('240')
+  const [theme, setTheme] = useState('light')
+  const [isColorPicking, setIsColorPicking] = useState(false)
+
+  useEffect(() => {
+    document.documentElement.setAttribute('color-scheme', theme)
+  }, [theme])
+
+  useEffect(() => {
+    document.documentElement.style.setProperty('--_hue', hue)
+  }, [hue])
+
+  return (
+    <aside
+      className={styles.wrapper}
+      style={{
+        backgroundColor: isColorPicking ? 'hsl(var(--muted) / .6' : 'transparent'
+      }}
+    >
+      {
+        isColorPicking ? (
+          <>
+            <button
+              className={`btn ${styles.close}`}
+              aria-label="Close color picking mode"
+              onClick={() => setIsColorPicking(false)}
+            >
+              <XMarkIcon />
+            </button>
+            <input 
+              className={styles.picker}
+              type="range" 
+              min="0"
+              max="360"
+              aria-label='Change color theme slider'
+              value={hue}
+              onInput={(e) => setHue(e.target.value)}    
+            />
+          </>
+        ) : (
+          <div className={styles.btns}>
+            <button 
+              className='btn'
+              aria-label={`Change theme to ${theme == "light" ? "dark" : "light"} mode`}
+              role="switch"
+              onClick={() => setTheme(theme == "light" ? "dark" : "light")}
+            >
+              {
+                theme == "light" ? <MoonIcon /> : <SunIcon />
+              }
+            </button>
+            <button 
+              className='btn'
+              aria-label="Enable color picking mode"
+              onClick={() => setIsColorPicking(true)}
+            >
+              <SwatchIcon />
+            </button>
+          </div>
+        )
+      }
+    </aside>
+  )
+}
+
+export default ThemeSwitcher
