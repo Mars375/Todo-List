f643b9af483addd37161465804d726708c97effe
Loic Rossi <loic_2610@hotmail.fr>
2023-03-01 15:17:36 +0100

    Custom hooks to save in LocalStorage

---
+++ src/hooks/useLocalStorage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = React.useState(() => {
+    try {
+    const storedValue = window.localStorage.getItem(key)
+    return storedValue ? JSON.parse(storedValue) : initialValue
+    } catch (error) {
+      console.log(error)
+      return initialValue
+    }
+  })
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value])
+
+  return [value, setValue]
+}
+
+export default useLocalStorage
